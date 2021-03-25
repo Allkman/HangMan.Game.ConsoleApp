e3b09d84d031f4614732f71c3b48e31eae496c48
Allkman(ADev) <algirdas.cern@gmail.com>
2021-03-25 16:43:50 +0200

    Line 86 -110. I am able to get words based on selected topic. Cant use Random(); to Select one KeyValuePair.Value (word)

--- HangMan.Game.ConsoleApp/Program.cs
+++ HangMan.Game.ConsoleApp/Program.cs
@@ -6,72 +6,62 @@ namespace HangMan.Game.ConsoleApp
 {
     class Program
     {
-        const string topicNames = "NAMES";
+        //UI Elements
+        #region 
+        const string topicNames = "NAMES";
         const string topicCities = "CITIES IN LITHUANIA";
         const string topicCountries = "COUNTRIES";
         const string topicFurnitures = "FURNITURE";
-        const int numberOfAttempts = 7;
-
+        const string askForTopicSelectTxt = "Choose topic: ";
+        const string wordTxt = "Word: ";
+        const string msgIncorrectTopic = "Input correct topic number";
+        const string txtTopic = "Topic: - ";
+        const string askReplayTxt = "Do you want to replay Y/N ?";
+        const string guessLetterOrFullWordTxt = "Guess letter or full word: ";
+        const string guessedLettersTxt = "\nGuessed letters: ";
+        const string txtNoMoreWord = "There are  no more words left in this topic";
+        static string winMessageTxt = @"!!! CONGRATULATION !!!! WORD IS CORRECT :)            
+            The word: ";
+        #endregion
+        const int numberOfAttempts = 7;
         readonly static List<string> topics = new List<string> { topicNames, topicCities, topicCountries, topicFurnitures };
         readonly static List<string> names = new List<string> { "Mindaugas", "Gediminas", "Vytautas", "Kestutis", "Algirdas", "Žygimantas", "Birute", "Barbora", "Augustas", "Morta" };
         readonly static List<string> cities = new List<string> { "Vilnius", "Klaipeda", "Ukmerge", "Taurage", "Alytus", "Palanga", "Utena", "Varena", "Kaunas", "Raseiniai" };
         readonly static List<string> countries = new List<string> { "Kinija", "Prancuzija", "Estija", "Norvegija", "Taivanas", "Indija", "Meksika", "Suomija", "Argentina", "Portugalija" };
         readonly static List<string> furniture = new List<string> { "Stalas", "Kede", "Spinta", "Lova", "Suolas", "Sofa", "Lempa", "Durys", "Kilimas", "Veidrodis" };
-        //Dictionary for used words so they wont repeat when replaying
-        //static Dictionary<string, List<string>> usedWords = new Dictionary<string, List<string>>();
-        public Random rnd = new Random();
+        readonly static List<List<string>> listOfWordsList = new List<List<string>> { names, cities, countries, furniture };
+        static Dictionary<string, List<string>> usedWords = new Dictionary<string, List<string>>();
 
         static void Main(string[] args)
         {
+            HangMan();
 
-            //    HangMan();
+        }
+        static void HangMan()
+        {
+            bool replay = true;
+            while (replay)
+            {
+                Console.Clear();
 
-            //TODO 1. Create a method bellow to display Topics selection. Enter number 1-4. +DONE
-
-            //Bellow code is for testing purposes only.. I will remove it once all mechanics are done.
-            var topic = TopicSelection();
-            Console.WriteLine();
-            //Console.Clear();
-            var word = RandomWordGenerator(topic, GetTopicAndWordsPaired(topic));
-
-
-            Console.WriteLine($"Number of word in selected topic: {word}");
-
-            Console.WriteLine();
-
-            var incorrectGuesses = new List<string>(); //adding incorrect guesses to a List, so that later i could compare it to number of attempts
-                                                       //var correctGuesses = new string[word.Length];
-
-            StartingPicture();
-
-            //replay = (Console.ReadKey().KeyChar.ToString().ToUpper() == "T");
-
-            //TODO 2. After Topic was selected, pick a word from a List randomly
-            //TODO 3 Display UI elements for a game
-            //TODO 3a. Allow user to enter letters.
-            //TODO 3b. Dislay Hanger
-            //TODO 3c. Update Hanger if user entred wrong letter(s)
-            //TODO 3d. Display wrongly guessed letters. This is needed to user, to be able to see what letters have been guessed. Restrict guessing to 7 times( 7 lives on Hanger)
-            //TODO 3e. Place correctly guessed letter into a UI: _ _ _ _ _ A _.
-            //TODO 4. If when the word guess
-            //TODO
-
-            Console.ReadKey();
+                List<KeyValuePair<string, string>> kvpOfAllWords = new List<KeyValuePair<string, string>>();
+                var topic = TopicSelection();
+                var word = RandomWordGenerator(topic, GetKVPOfAllWords(kvpOfAllWords));
+                if (word == null)
+                {
+                    Console.WriteLine(txtNoMoreWord);
+                }
+                else
+                {
+                    PlayGame(topic, word);
+                }
+                replay = (Console.ReadKey().KeyChar.ToString().ToUpper() == "T");
+            }
         }
 
-        //TODO GamePlay Method
-        static void HangMan()
+        static string TopicSelection()
         {
-            //var topic = TopicSelection();
-            // var playWord = RandomWordGenerator();
-        }
-
-        //TODO Game mechanics
-        //TODO I - Show Topics and let user select topic from list of topics
-
-        static string TopicSelection()
-        {
-            Console.WriteLine("Choose one of the following topics: ");
+            Console.WriteLine(askForTopicSelectTxt);
             var topicNumber = 0;
             DisplayTopicNames();
             while (topicNumber > topics.Count || topicNumber == 0)
@@ -80,10 +70,8 @@ namespace HangMan.Game.ConsoleApp
                 int.TryParse(topicInput.ToString(), out topicNumber);
                 if (topicNumber > topics.Count || topicNumber == 0)
                 {
-
-                    Console.WriteLine("Please enter a number representing a topic number");
+                    Console.WriteLine(msgIncorrectTopic);
                 }
-
             }
             return topics[topicNumber - 1];
         }
@@ -91,88 +79,166 @@ namespace HangMan.Game.ConsoleApp
         {
             for (int i = 1; i <= topics.Count; i++)
             {
-
-                Console.Write("\n Topic - {0}: = {1}", i, topics[i - 1]);
+                Console.Write("\n {0} - {1}: = {2}", txtTopic, i, topics[i - 1]);
             }
             Console.WriteLine();
         }
-        //TODO II. A Method to get randomly selected word for selected topic
-        static string RandomWordGenerator(string topicForKvp, KeyValuePair<string, string> kvpTopicAndWords)
+        static string RandomWordGenerator(string topic, List<KeyValuePair<string, string>> kvpOfAllWords)
         {
+            var wordFromKVP = new KeyValuePair<string, string>();
+            var rnd = new Random();
+            foreach (var kvp in kvpOfAllWords)
+            {
+                if (kvp.Key == topic)
+                {
+                    wordFromKVP = kvp;
+                    Console.WriteLine($"kvp is = {kvp}"); //Display all pairs from selected topic (in this case 10)
+                }
+            }
+            return wordFromKVP.Value;
+        }
+        static List<KeyValuePair<string, string>> GetKVPOfAllWords(List<KeyValuePair<string, string>> kvpOfAllWords)
+        {
+            for (int i = 0; i < listOfWordsList.Count; i++)
+            {
+                foreach (var word in listOfWordsList[i])
+                {
+                    kvpOfAllWords.Add(new KeyValuePair<string, string>(topics[i], word));//all topics and words paired
+                }
+            }
+            return kvpOfAllWords;
+        }
+        static void PlayGame(string topic, string word)
+        {
+            var incorrectGuesses = new List<string>();
+            var correctGuesses = new string[word.Length];
 
-            Random rnd = new Random();
-            var randomSelect = topicForKvp;
+            FillUsedWordsList(topic, word);
 
-            randomSelect = rnd.Next(0, kvpTopicAndWords.Value.Length).ToString();
-
-
-            return randomSelect;
-
+            StartingPicture();
+            WordGuessField(correctGuesses);
+            bool guessingAllowed = true;
+            while (guessingAllowed)
+            {
+                string guess = DisplayLetterOrWordGuessing();
+                bool wasItWordGuess = guess.Length > 1;
+                if (wasItWordGuess)
+                {
+                    WordGuesssing(guessingAllowed, word, guess);
+                }
+                else
+                {
+                    LetterGuessing(incorrectGuesses, correctGuesses, guessingAllowed, guess, word);
+                }
+                Console.WriteLine(askReplayTxt);
+            }
+        }
+        static void FillUsedWordsList(string topic, string word)
+        {
+            if (usedWords.ContainsKey(topic)) usedWords[topic].Add(word);
+            else usedWords.Add(topic, new List<string> { word });
         }
 
-        static KeyValuePair<string, string> GetTopicAndWordsPaired(string topicForKvp)
-        {
-            var topicIndex = topicForKvp;
-            List<List<string>> listOfWordsLists = new List<List<string>> { names, cities, countries, furniture };
-
-            foreach (var list in listOfWordsLists)
-            {
-                for (int i = 0; i < listOfWordsLists.Count; i++)
-                {
-                    topicIndex = list[i];
-                }
-            }
-            var kvpWords = new KeyValuePair<string, string>();
-            foreach (var words in listOfWordsLists)
-            {
-
-                for (int i = 0; i < words.Count; i++)
-                {
-                    kvpWords = new KeyValuePair<string, string>((string)topicForKvp, words[i]);
-                }
-            }
-            Console.WriteLine($"Selected Topic: {topicForKvp}");
-            return kvpWords;
-        }
-
-        //TODO III. A Method
-        //TODO IV. A Method to place Words based on their Topic into a Dictionary. For Word that havent beed used yet.
-        //TODO V. A Method to place used word into a List
-
-
-        //static List<string> PutUsedWordsToList(List<string> inputOfUsedWord, string topic)
-        //{
-        //    var result = new List<string>();
-        //    foreach (var word in inputOfUsedWord)
-        //    {
-        //        if (usedWords.ContainsKey(topic))
-        //        {
-        //            result.Add(word);
-        //        }
-        //    }
-        //    return result;
-        //}
-
-        static void WordGuessField(string[] word)
+        static void WordGuessField(string[] correctGuesses)
         {
             Console.WriteLine();
-            StringBuilder displayToPlayer = new StringBuilder(word.Length);
+            var sb = new StringBuilder(wordTxt);
+            foreach (var letter in correctGuesses)
+            {
+                if (string.IsNullOrWhiteSpace(letter)) sb.Append("_ ");
+                else sb.Append($"{letter} ");
+            }
+            var result = sb.ToString();
+            Console.WriteLine(result);
+        }
+        static string DisplayLetterOrWordGuessing()
+        {
+            Console.WriteLine("\n\n{0}", guessLetterOrFullWordTxt);
+            return Console.ReadLine();
+        }
+        static void WordGuesssing(bool guessingAllowed, string guess, string word)
+        {
+            bool wasItWord = word == guess;
+
+            if (wasItWord)
+            {
+                DisplayWinMessage(word);
+            }
+            else
+            {
+                DisplayWithLeg2(word);
+            }
+            guessingAllowed = false;
+        }
+        static void LetterGuessing(List<string> incorrectGuesses, string[] correctGuesses, bool guessingAllowed, string guess, string word)
+        {
+            bool wasItLetter = incorrectGuesses.Contains(guess);
+            if (!wasItLetter)
+            {
+                var letterIndexes = CheckLettersAndReturnIndexes(word, guess);
+                if (letterIndexes.Count == 0)
+                {
+                    incorrectGuesses.Add(guess);
+                }
+                else
+                {
+                    PlaceLetterToItsIndex(guess, letterIndexes, ref correctGuesses);
+                }
+            }
+            if (incorrectGuesses.Count == numberOfAttempts)
+            {
+                DisplayWithLeg2(word);
+                guessingAllowed = false;
+            }
+            else
+            {
+                HangerPictureSelection(incorrectGuesses);
+                DisplayIncorrectlyGuessedLetters(incorrectGuesses);
+                WordGuessField(correctGuesses);
+                bool a = WasWordGuessed(correctGuesses, word);
+                if (a == true)
+                {
+                    DisplayWinMessage(word);
+                    guessingAllowed = false;
+                }
+            }
+        }
+        static bool WasWordGuessed(string[] correctGuesses, string word)
+        {
+            string a = string.Join("", correctGuesses);
+            return a.ToUpper() == word.ToUpper();
+        }
+        static List<int> CheckLettersAndReturnIndexes(string word, string guess)
+        {
+            var wordArray = word.ToCharArray();
+            var result = new List<int>();
             for (int i = 0; i < word.Length; i++)
-                displayToPlayer.Append('_');
+            {
+                if (wordArray[i].ToString().ToUpper() == guess.ToUpper()) result.Add(i);
+            }
+            return result;
         }
-
-
-
-        //TODO VI. A Method to create UI to display Word guessing field. (String builder)
-        //TODO VII. A Method to let write  a Letter
-        //TODO VIII. A Method to check if a given word has a letter.
-        //TOTO IX. A Method to place written letter into a correct place in a given word 
-        //TODO X. A Method to display all wrongly guessed letter.
-        //TODO XI. A Method to check if a word was guessed.
-        //TODO XII. A Method to display WIN message (+ a word)
-
-        //TOTO XIII. A Method to select Hangman UI + UI elements                                                               +DONE
-        static void HangerPictureSelection(List<string> incorrectGuesses)
+        static void PlaceLetterToItsIndex(string spejimas, List<int> raidesIndeksai, ref string[] teisingiSpejimai)
+        {
+            foreach (int indeksas in raidesIndeksai)
+            {
+                teisingiSpejimai[indeksas] = spejimas;
+            }
+        }
+        static void DisplayIncorrectlyGuessedLetters(List<string> incorrectGuesses)
+        {
+            Console.WriteLine(guessedLettersTxt);
+            foreach (var incorrectGuess in incorrectGuesses)
+            {
+                Console.Write($"[{incorrectGuess}] ");
+            }
+        }
+        static void DisplayWinMessage(string word)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(winMessageTxt + word);
+        }
+        static void HangerPictureSelection(List<string> incorrectGuesses)
         {
             Console.Clear();
             switch (incorrectGuesses.Count)
